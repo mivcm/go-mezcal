65e62de5c3150975abb3162952d67acd2872e99c
FerchoVJContalink <fernando.viveros@contalink.com>
2025-08-19 15:29:17 -0600

    Validate length featured products

--- components/featured-products.tsx
+++ components/featured-products.tsx
@@ -5,23 +5,27 @@ import { useProductsStore } from "@/hooks/use-products-store";
 import { useEffect } from "react";
 
 export default function FeaturedProducts() {
-    const { products, getFeaturedProducts } = useProductsStore();
+  const { products, getFeaturedProducts } = useProductsStore();
 
-    useEffect(() => {
-        getFeaturedProducts();
-    }, [getFeaturedProducts]);
+  useEffect(() => {
+    getFeaturedProducts();
+  }, [getFeaturedProducts]);
 
-    return (
-        <section className="py-16 bg-amber-50/50 dark:bg-stone-900">
-        <div className="container">
-          <div className="flex flex-col sm:flex-row justify-between items-baseline mb-8">
-            <h2 className="text-3xl font-bold">Mezcales Destacados</h2>
-            <Link href="/productos" className="text-amber-600 hover:text-amber-700 font-medium">
-              Ver todos los mezcales
-            </Link>
-          </div>
-          <ProductList products={products} />
+  if (products.length === 0) {
+    return null;
+  }
+
+  return (
+    <section className="py-16 bg-amber-50/50 dark:bg-stone-900">
+      <div className="container">
+        <div className="flex flex-col sm:flex-row justify-between items-baseline mb-8">
+          <h2 className="text-3xl font-bold">Mezcales Destacados</h2>
+          <Link href="/productos" className="text-amber-600 hover:text-amber-700 font-medium">
+            Ver todos los mezcales
+          </Link>
         </div>
-      </section>
-    )
+        <ProductList products={products} />
+      </div>
+    </section>
+  )
 }
